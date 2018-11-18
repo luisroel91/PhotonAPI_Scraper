1a06cce01663ae024c354923447146fe912b5407
Luis Roel <boundbygravity12@gmail.com>
2018-11-18 17:58:06 -0500

    can grab coord correctly

--- __int__.py
+++ __int__.py
@@ -1,16 +1,26 @@
 import pandas as pd
-import numpy as np
+import time
 import photonlib
-import json
-from pandas.io.json import json_normalize
 
-rows_per_query = 2
+rows_per_query = 6500
+time_per_batch = 30
 biz_names = photonlib.load_biznames()
 data_sink = pd.DataFrame()
 
 for biz in biz_names:
+    response = photonlib.BlockObject(biz, rows_per_query).render_frame()
 
-    query_json = photonlib.get_data(query=biz,)
+    data_sink = pd.concat([data_sink, response], ignore_index=True, sort=True)
 
+    print(
+        'Finished batch of {:d} for {:s}. {:d} records/{:d} categories total.\nWaiting {:d} seconds before next batch.'.format(
+            rows_per_query,
+            biz,
+            data_sink.shape[0],
+            data_sink.shape[1],
+            time_per_batch
+        ))
 
+    time.sleep(time_per_batch)
 
+data_sink.to_csv('output.csv')
--- photonlib.py
+++ photonlib.py
@@ -1,7 +1,5 @@
 import requests
 import pandas as pd
-from pandas.io.json import json_normalize
-import time
 
 
 def load_biznames():
@@ -15,10 +13,6 @@ def url_constructor(path):
     return 'http://photon.komoot.de/api/?' + path
 
 
-def unpack_frame():
-    pass
-
-
 class BlockObject(object):
 
     def __init__(self, query: str, block_size: int):
@@ -26,47 +20,31 @@ class BlockObject(object):
         self.block_size = block_size
 
         self.raw_data = requests.get(
-            url_constructor('q=\'{:s}\'&limit={:d}'.format(
-                self.query,
-                self.block_size
-            ))).json()
-
+            url_constructor('q=\'{:s}\'&limit={:d}'.format(self.query, self.block_size))).json()
         self.features = self.raw_data['features']
+        self.features_frame = pd.DataFrame(self.features)
+        self.geometry = self.features_frame['geometry']
+        self.properties = self.features_frame['properties']
 
-        print(self.raw_data)
-
-    def generate_frame(self):
+    def render_frame(self):
         features_frame = pd.DataFrame(self.features)
         features_frame = features_frame.drop(columns='type')
 
-        scrape_frame = pd.DataFrame()
+        properties_frame = pd.DataFrame()
+        property_keys = []
 
-        for i, key in features_frame.iterrows():
+        rendered_frame = pd.DataFrame()
 
-            # for geokey in features_frame.geometry[i]:
-        scrape_frame[key] = features_frame.geometry.apply(lambda x: x[key])
+        rendered_frame['coordinates'] = self.geometry.apply(lambda x: x['coordinates'])
 
-            for key in features_frame.properties[i]:
-                properties_frame[key] = features_frame.properties.apply(lambda x: x[key])
+        for key in self.properties:
+            try:
+                properties_frame[key] = self.properties.apply(lambda x: x[key])
 
-        rendered_frame = properties_frame.join(geometry_frame)
-        rendered_frame = rendered_frame.fillna('NaN')
+            except KeyError:
+                properties_frame[key] = "This is a key error"
+                print("\nKey Error\n")
+
+        rendered_frame = rendered_frame.join(properties_frame)
 
         return rendered_frame
-
-        '''
-         for key in features_frame.properties[i]:
-                properties_frame[key] = features_frame.properties.apply(lambda x: x[key])
-        
-        
-        for counter, row in enumerate(features_frame.iterrows()):
-        element_name = row
-
-        print(element_name, counter)
-        feature_coord = row['geometry'][index]['coordinates']
-        feature_name = features_frame['']
-        '''
-
-
-response = BlockObject('Home Depot', block_size=2)
-print(response.generate_frame())
